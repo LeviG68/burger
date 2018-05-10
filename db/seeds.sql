72f79727395ec822bd839a6ef22759ccc66522bc
levi <glendenning_levi@hotmail.com>
2018-05-09 18:13:50 -0600

    start of project

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_db) VALUES ('Cheese Burger');
+INSERT INTO burgers (burgers_db) VALUES ('Bacon Mushroom Burger');
+INSERT INTO burgers (burgers_db) VALUES ('Buffalo Burger');
